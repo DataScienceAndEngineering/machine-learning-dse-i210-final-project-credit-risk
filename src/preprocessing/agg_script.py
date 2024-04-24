f6a4d6cda5525f77477d45ecaecdb48d84336fa9
Wayne Lam <w.p.lam107@gmail.com>
2024-04-24 14:11:28 -0400

    aggs update

--- src/preprocessing/agg_script.py
+++ src/preprocessing/agg_script.py
@@ -2,12 +2,12 @@ import polars as pl
 import glob
 import os
 
-from utils.merge import group_file_data
+import utils.merge as M
 
 BASE_DIR = 'data/raw/parquet_files/train/'
 FILE_GROUPS = [
-    'train_static_0', 'train_static_cb', 'train_tax_registry_a',
-    'train_tax_registry_b', 'train_tax_registry_c',
+    # 'train_static_0', 'train_static_cb', 
+    'train_tax_registry_a', 'train_tax_registry_b', 'train_tax_registry_c',
     'train_applprev_1', 'train_applprev_2',
     'train_credit_bureau_a_1', 'train_credit_bureau_a_2',
     'train_credit_bureau_b_1', 'train_credit_bureau_b_2',
@@ -32,14 +32,17 @@ if __name__ == '__main__':
         # Get columns
         df = pl.DataFrame()
         for file in files:
-            temp_df = pl.read_parquet(file)
+            temp_df = pl.read_parquet(file).pipe(M.set_table_dtypes)
             df = pl.concat([df, temp_df])
 
         # Date columns
-        date_cols = [ df.columns[i] for i in range(len(df.columns)) if (df.columns[i].__contains__('dat')) and (df.dtypes[i] == pl.String) ]
+        date_cols = [ df.columns[i] for i in range(len(df.columns)) if df.dtypes[i] == pl.Date ]
 
         # Categorical columns
-        cat_cols = [ df.columns[i] for i in range(len(df.columns)) if (df.columns[i] not in date_cols) and (df.dtypes[i] == pl.String) ]
+        cat_cols = [ 
+            df.columns[i] for i in range(len(df.columns)) 
+            if df.dtypes[i] == pl.String
+        ]
 
         # Numerical columns
         ignore_cols = ['case_id', 'num_group1', 'num_group2']
@@ -48,19 +51,46 @@ if __name__ == '__main__':
             if (df.columns[i] not in date_cols) and (df.columns[i] not in cat_cols) and (df.columns[i] not in ignore_cols)
         ]
 
-        # Drop cat cols
-        df = df.drop(cat_cols)
+        # Extract year, month, day
+        # date_df = M.separate_dates(df, date_cols)
+
+        # Frequency encode cat columns
+        # freq_df = M.freq_encoder(df, cat_cols)
+
+        # Binary encode cat columns
+        # bin_df = M.binary_encoder(df, cat_cols)
 
         # Create and write DataFrames
         if 'num_group1' in df.columns:
             df_num_group_0 = df.filter(pl.col('num_group1') == 0)
             df_num_group_rest = df.filter(pl.col('num_group1') != 0)
 
-            df_num_group_0 = group_file_data(df_num_group_0, num_cols, date_cols)
-            df_num_group_rest = group_file_data(df_num_group_rest, num_cols, date_cols)
+            df_num_group_0 = M.group_file_data(df_num_group_0, num_cols, date_cols, cat_cols)
+            df_num_group_rest = M.group_file_data(df_num_group_rest, num_cols, date_cols, cat_cols)
 
-            output_file_0 = df_num_group_0.write_parquet(os.path.join(OUTPUT_DIR, file_group + '_grouped_0.parquet'))
-            output_file_rest = df_num_group_rest.write_parquet(os.path.join(OUTPUT_DIR, file_group + '_grouped_rest.parquet'))
+            new_date_cols = [ 
+                col for col in df_num_group_0.columns 
+                if df_num_group_0[col].dtype == pl.Date 
+            ]
+
+            # Transform data
+            date_df_0 = M.separate_dates(df_num_group_0, new_date_cols)
+            date_df_rest = M.separate_dates(df_num_group_rest, new_date_cols)
+            freq_df_0 = M.freq_encoder(df_num_group_0, cat_cols)
+            freq_df_rest = M.freq_encoder(df_num_group_rest, cat_cols)
+            bin_df_0 = M.binary_encoder(df_num_group_0, cat_cols)
+            bin_df_rest = M.binary_encoder(df_num_group_rest, cat_cols)
+
+            # Merge data
+            df_0 = pl.concat([df_num_group_0, date_df_0, freq_df_0, bin_df_0], how='horizontal')
+            df_rest = pl.concat([df_num_group_rest, date_df_rest, freq_df_rest, bin_df_rest], how='horizontal')
+
+            # Drop unprocessed columns
+            df_0 = df_0.drop(date_cols + cat_cols)
+            df_rest = df_rest.drop(date_cols + cat_cols)
+
+            output_file_0 = df_0.write_parquet(os.path.join(OUTPUT_DIR, file_group + '_grouped_0.parquet'))
+            output_file_rest = df_rest.write_parquet(os.path.join(OUTPUT_DIR, file_group + '_grouped_rest.parquet'))
         else:
-            df = group_file_data(df, num_cols, date_cols)
+            df = M.group_file_data(df, num_cols, date_cols)
             output_file = df.write_parquet(os.path.join(OUTPUT_DIR, file_group + '_grouped.parquet'))
--- src/preprocessing/utils/merge.py
+++ src/preprocessing/utils/merge.py
@@ -1,4 +1,86 @@
 import polars as pl
+from sklearn.preprocessing import LabelEncoder
+
+def set_table_dtypes(df: pl.DataFrame) -> pl.DataFrame:
+    # implement here all desired dtypes for tables
+    # the following is just an example
+    for col in df.columns:
+        # last letter of column name will help you determine the type
+        if col[-1] in ("P", "A"):
+            df = df.with_columns(pl.col(col).cast(pl.Float64).alias(col))
+
+        if col[-1] == 'D':
+            df = df.with_columns(pl.col(col).str.to_date())
+
+    return df
+
+def separate_dates(df: pl.DataFrame, date_cols: list[str] = []) -> pl.DataFrame:
+
+    date_df = df.select(date_cols)
+
+    for col in date_cols:
+        date_df = date_df.with_columns(pl.col(col).dt.year().alias(col + '_year'))
+        date_df = date_df.with_columns(pl.col(col).dt.month().alias(col + '_month'))
+        date_df = date_df.with_columns(pl.col(col).dt.day().alias(col + '_day'))
+        date_df.drop_in_place(col)
+
+    return date_df
+
+def freq_encoder(df: pl.DataFrame, cat_cols: list[str] = []) -> pl.DataFrame:
+
+    cat_df = df.select(cat_cols)
+
+    for col in cat_cols:
+        # Calculate frequency for each category in the column
+        value_counts = cat_df.groupby(col).agg(pl.len().alias('count'))
+        total_count = cat_df.height  # Use height for row count in Polars
+        frequency = (value_counts.with_columns(
+                        (value_counts['count'] / total_count).alias(f'{col}_freq')
+                    ).select([col, f'{col}_freq']))
+        
+        # Joining the frequency DataFrame back to the original DataFrame
+        cat_df = cat_df.join(frequency, on=col, how='left')
+        cat_df.drop_in_place(col)
+
+    return cat_df
+
+def binary_encoder(df: pl.DataFrame, cat_cols: list[str] = []) -> pl.DataFrame:
+    # Initialize LabelEncoder
+    le = LabelEncoder()
+
+    cat_df = df.select(cat_cols)
+
+    # Binary encoding for each categorical column
+    for col in cat_cols:
+        # Convert categories to integers using LabelEncoder from sklearn
+        encoded_int = le.fit_transform(cat_df[col].to_numpy())
+
+        # Convert the numpy array back to a Polars Series and rename it
+        int_col_name = f"{col}_int"
+        encoded_series = pl.Series(encoded_int).alias(int_col_name)
+
+        # Add the integer encoded column to the DataFrame
+        cat_df = cat_df.with_columns(encoded_series)
+
+        # Calculate the maximum binary length
+        max_binary_length = encoded_series.max().bit_length()
+
+        # Create binary encoding directly
+        for bit_position in range(max_binary_length):
+            # Use bitwise operations directly within Polars
+            bit_value = (encoded_series / (2 ** bit_position)).cast(pl.Int64) & 1
+            new_col_name = f"{col}_binary_{bit_position}"
+            cat_df = cat_df.with_columns(
+                bit_value.alias(new_col_name)
+            )
+            cat_df = cat_df.with_columns(
+                pl.col(new_col_name).cast(pl.Int8)
+            )
+
+        cat_df.drop_in_place(int_col_name)
+        cat_df.drop_in_place(col)
+    
+    return cat_df
 
 def group_file_data(
     df: pl.DataFrame, 
@@ -18,7 +100,7 @@ def group_file_data(
     '''
     
     # Convert date columns
-    df_date = df[['case_id'] + date_cols].with_columns([ pl.col(col).str.to_date() for col in date_cols ])
+    df_date = df[['case_id'] + date_cols]
 
     # One-hot categories
     # df_dummies = df[['case_id'] + cat_cols].to_dummies(cat_cols)
@@ -40,8 +122,12 @@ def group_file_data(
 
     # Cat aggs
     # cat_aggs = [ pl.col(col).mode().name.suffix('_mode') for col in cat_cols ] +\
-    #            [ pl.n_unique(col).name.suffix('_distinct') for col in cat_cols ]
-    # df_cat_grouped = df_cat.group_by('case_id').agg(cat_aggs)
+            #    [ pl.n_unique(col).name.suffix('_distinct') for col in cat_cols ]
+    cat_aggs = [ pl.col(col).mode() for col in cat_cols ]
+    df_cat_grouped = df_cat.group_by('case_id').agg(cat_aggs)
+    for col in df_cat_grouped.columns:
+        if df_cat_grouped[col].dtype != pl.Int64:
+            df_cat_grouped = df_cat_grouped.with_columns(pl.col(col).map_elements(lambda x: x[0], return_dtype=pl.String))
 
     # Numerical aggs
     num_aggs = [ pl.min(col).name.suffix('_min') for col in num_cols ] +\
@@ -54,6 +140,6 @@ def group_file_data(
     # Join DataFrames
     df_joined = df_num_grouped.join(df_date_grouped, on='case_id')
     # df_joined = df_joined.join(df_dummies_grouped, on='case_id')
-    # df_joined = df_joined.join(df_cat_grouped, on='case_id')
+    df_joined = df_joined.join(df_cat_grouped, on='case_id')
 
     return df_joined
